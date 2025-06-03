eeac6203eac91a2d6acaa86739ce392a37c4b533
prof. Marnei Cardoso <marneicardoso.geracaotech@gmail.com>
2025-06-03 16:47:47 -0300

    Restore de branch

---
+++ src/routes/user.routes.js
@@ -0,0 +1,14 @@
+import express from "express";
+import controller from "../controllers/userController";
+import auth from "../middlewares/auth";
+
+const router = express.Router();
+
+router.post("/register", controller.registerUser);
+router.post("/login", controller.authentication);
+
+router.get("/", auth, controller.listUsers);
+router.put("/:id", auth, controller.updateUser);
+router.delete("/:id", auth, controller.deleteUser);
+
+export default router;
