713dcdf384db5ef9bdd11b3950c9eeab68178fff
prof. Marnei Cardoso <marneicardoso.geracaotech@gmail.com>
2025-06-03 15:40:47 -0300

    feat/user.routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -4,4 +4,11 @@ import auth from "../middlewares/auth";
 
 const router = express.Router();
 
+router.post("/register", controller.registerUser);
+router.post("/login", controller.authentication);
+
 router.get("/", auth, controller.listUsers);
+router.put("/:id", auth, controller.updateUser);
+router.delete("/:id", auth, controller.deleteUser);
+
+export default router;
